816c9b85908aa3a513f0602771365b817b4767dc
Thanos <61045882+KokkinosThanos@users.noreply.github.com>
2021-10-11 20:19:12 +0300

    Update mysql.sql

--- database/mysql/mysql.sql
+++ database/mysql/mysql.sql
@@ -1,11 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS friends;
 
--- GRANT ALL PRIVILEGES ON friends.* TO 'jupiter'@'%' IDENTIFIED BY 'secret';
--- GRANT ALL PRIVILEGES ON friends.* TO 'jupiter'@'localhost' IDENTIFIED BY 'secret';
-
--- CREATE USER 'jupiter'@'localhost' IDENTIFIED BY 'secret';
--- GRANT ALL PRIVILEGES ON friends.* TO 'jupiter'@'localhost';
-
 USE friends;
 -- 
 --
@@ -17,15 +11,3 @@ CREATE TABLE friends(
     age INT NOT NULL,
     PRIMARY KEY (id)
 );
-
--- 
-
--- INSERT INTO friends(firstname, lastname, phone, age)
--- VALUES
--- ("John", "Doe", 3244335, 30),
--- ("Mark", "Bi", 1238477, 36),
--- ("Lao", "Che", 3200981, 28),
--- ("Thanos", "Takos", 7779008, 39),
--- ("Great", "Day", 12865443, 24),
--- ("Harilaos", "Sao", 111223, 58),
--- ("Giorgos", "Nafsakos", 111223, 25);
